712b720aa82e878330a73f3ecaaf4f94f31d4e73
ScottAgirs <scott@ijs.to>
2021-04-22 01:21:52 -0400

    feat: Add lesson files for 4-5
    Signed-off-by: ScottAgirs <scott@ijs.to>

---
+++ lesson-changes/4-5/api/trip/controllers/trip.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  create: (ctx) => {
+    console.log('ctx.request.body', ctx.request.body);
+
+    
+  }
+};
